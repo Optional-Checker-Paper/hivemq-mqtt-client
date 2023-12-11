1f7c71fc80f91a2800799ad05643c5cc013f8930
James Yoo <jyoo980@gmail.com>
2023-12-10 16:59:26 -0800

    Adding -AassumeAssertionsAreEnabled flag

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,7 +72,8 @@ configure<CheckerFrameworkExtension> {
     extraJavacArgs = listOf(
             "-AsuppressWarnings=type.anno.before.modifier,type.anno.before.decl.anno",
             "-AassumePure",
-            "-AwarnUnneededSuppressions"
+            "-AwarnUnneededSuppressions",
+            "-AassumeAssertionsAreEnabled"
     )
     excludeTests = true
     val checkerFrameworkVersion = "3.41.1-SNAPSHOT"
